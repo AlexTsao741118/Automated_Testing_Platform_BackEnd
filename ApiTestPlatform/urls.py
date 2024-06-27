ddd5a1531e9489cdff79b136e78d9317484eba4b
AlexTsao741118 <andy741118@gmail.com>
2024-06-28 00:56:49 +0800

    Update current development progress

--- ApiTestPlatform/urls.py
+++ ApiTestPlatform/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView
+from users.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # 登录接口的访问路径
-    path('bryant-admin/users/login', TokenObtainPairView.as_view(), name='login'),
+    # path('bryant-admin/users/login', TokenObtainPairView.as_view(), name='login'),
+    path('bryant-admin/users/login', LoginView.as_view(), name='login')
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.    
+# Create your views here.
+
+class LoginView(TokenObtainPairView):
+    """自定義登錄返回的數據"""
+
+    def post(self, request: Request, *args, **kwargs) -> Response:
+        serializer = self.get_serializer(data=request.data)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as e:
+            raise InvalidToken(e.args[0])
+        # 自定義登錄返回的字段
+        result = serializer.validated_data
+        result_data = {}
+        result_data['token'] = result['access']
+        result_data['refresh'] = result['refresh']
+
+        return Response(result_data, status=status.HTTP_200_OK)
+    
